a09c0648a71abea722dc9e15b435ac9db7f70a6f
Ambre <104760273+AmbivalenceR@users.noreply.github.com>
2023-05-18 19:57:23 +0200

    start home design

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
+body {
+  margin: 0;
+  background: linear-gradient(
+    to bottom,
+    rgba(255, 42, 42, 0.8),
+    rgba(255, 120, 0, 0.8),
+    rgba(255, 197, 21, 0.8),
+    rgba(191, 255, 241, 0.8)
+  );
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -14,7 +23,9 @@
   text-align: center;
   color: #2c3e50;
 }
-
+.bgBlack {
+  background: #2b2b2b;
+}
 nav {
   padding: 30px;
 }
---
+++ client/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bgBlack header">
+    <div class="logo">
+      <img alt="Vue logo" src="../assets/logo.png" />
+    </div>
+    <div>
+      <button>Inscription</button>
+      <button>Connexion</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 50px;
+}
+
+.logo {
+  width: 15%;
+  display: flex;
+}
+
+.logo img {
+  width: 100%;
+}
+</style>
