f40ab9634507501605d4e670152bedcb88d13b3e
matthieuFmt <matt136@hotmail.fr>
2023-05-22 21:51:40 +0200

    add migration

---
+++ api/database/migrations/3_create_albums_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('albums', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('artist_id');
+            $table->foreign('artist_id')->references('id')->on('artists');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('albums');
+    }
+};
