a19b585e059db786209275b9fc66b93d28362d97
Alaric-Gautier <92737965+Alaric-Gautier@users.noreply.github.com>
2023-06-02 15:10:57 +0200

    Add script

--- api/app/Console/Commands/importDataToDb.php
+++ api/app/Console/Commands/importDataToDb.php
@@ -2,7 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Album;
+use App\Models\Style;
+use App\Models\Track;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class importDataToDb extends Command
 {
@@ -12,7 +16,7 @@ class importDataToDb extends Command
 
     public function handle()
     {
-        $datasJson = file_get_contents('/api/app/Console/Commands/amplifireData.json');
+        $datasJson = file_get_contents(storage_path('data.json'));
         $datas = json_decode($datasJson, true);
 
         foreach ($datas['albums'] as $albumData) {
@@ -33,7 +37,9 @@ class importDataToDb extends Command
                     'title' => $trackData['title'],
                     'duration' => $trackData['duration'],
                     'file' => $trackData['file'],
+                    'album_id' => $album->id,
                 ]);
+                $album->tracks()->attach($track);
             }
         }
     }
---
+++ api/app/Models/Account.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use App\Notifications\CustomResetPasswordNotification;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Account extends Authenticatable
+{
+    use HasFactory, Notifiable, HasApiTokens;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $fillable = [
+        'nickname',
+        'firstname',
+        'lastname',
+        'email',
+        'password',
+    ];
+
+    /**
+     * 
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
+     * 
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new CustomResetPasswordNotification($token));
+    }
+}
